b7dbb2b7a2022b7e8d10ccafcaab38b231863a0a
Nestor04 <41174490+Nestor04@users.noreply.github.com>
2024-12-05 23:45:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+// Cambia dinámicamente tu nombre en el portafolio
+const userName = document.getElementById('user-name');
+userName.textContent = "Nestor Andres Mojica Reyes, Matricula 100365598  Lab programacion III"; // Cambia por tu nombre
